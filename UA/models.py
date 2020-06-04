bf8f20a09a3097f5c24fb2bf5795c2de8b57d079
tuhindewan <tuhinsshadow@gmail.com>
2020-06-04 15:49:41 +0600

    Create new class UserInfo in UA models

--- UA/models.py
+++ UA/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    facebook_link = models.URLField(blank=True)
+    profile_pic = models.ImageField(upload_to='profile_pics', blank=True) 
