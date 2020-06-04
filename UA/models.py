4c0dfa90b225a42e4a52532f99fa284dc7c50f57
tuhindewan <tuhinsshadow@gmail.com>
2020-06-04 16:20:43 +0600

    Register UserInfo model and run migration

--- UA/models.py
+++ UA/models.py
@@ -7,3 +7,6 @@ class UserInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     facebook_link = models.URLField(blank=True)
     profile_pic = models.ImageField(upload_to='profile_pics', blank=True) 
+
+    def __str__(self):
+        return UserInfo.user.username
