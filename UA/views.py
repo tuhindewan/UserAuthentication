44f8efd3e66a09282ca9fe0644254c5428d6ef9b
tuhindewan <tuhinsshadow@gmail.com>
2020-06-07 12:46:20 +0600

    Registration:   - complete functionality for user registration

--- UA/views.py
+++ UA/views.py
@@ -12,8 +12,32 @@ def index(request):
 def registration(request):
     userForm = forms.UserForm()
     userInfoForm = forms.UserInfoForm()
+    registered = False
+    
+    if request.method == 'POST':
+        userFormData = forms.UserForm(data=request.POST)
+        userInfoFormData = forms.UserInfoForm(data=request.POST)
+
+        if userFormData.is_valid() and userInfoFormData.is_valid():
+            user = userFormData.save()
+            user.set_password(user.password)
+            user.save()
+
+            user_info = userInfoFormData.save(commit=False)
+            user_info.user = user
+
+            if 'profile_pic' in request.FILES:
+                user_info.profile_pic = request.FILES['profile_pic']
+
+            user_info.save()
+            registered = True    
+    # else:
+    #     userForm = forms.UserForm()
+    #     userInfoForm = forms.UserInfoForm()
+
     dictionary = {
         'userForm': userForm,
         'userInfoForm': userInfoForm,
+        'registered': registered,
     }
     return render(request, 'ua/registration.html', context=dictionary)   
